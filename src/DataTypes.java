be7360a4464764e845b6291d800ab9a38cbe29db
yerx <yer.x.xiong@gmail.com>
2020-08-24 16:01:07 -0400

    first commit

---
+++ src/DataTypes.java
@@ -0,0 +1,33 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        boolean isRaining;
+
+        boolean isSunny = true;
+
+        byte b = 0;
+
+        int myNumber = 89;
+
+        char singleCharacter = 'a';
+
+        String myFirstName = "Yer";
+
+        String myLastName = "Xiong";
+
+        double myDoubleVariable = 0.9;
+
+        System.out.println("First value of singleCharacter: " + singleCharacter);
+
+        singleCharacter = 'y';
+
+        System.out.println("Second value of singleCharacter: " + singleCharacter);
+
+        System.out.println("Is it sunny? " + isSunny);
+
+        System.out.println("My number variable plus 1 is: " + (myNumber + 1));
+
+        System.out.println("My full name is " + myFirstName + " " + myLastName);
+
+        System.out.println("My double is " + myDoubleVariable);
+    }
+}
